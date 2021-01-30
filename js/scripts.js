b98e1b18e81eb6f197a9b814d81e78e2fe8ec11e
Miguel Bernal <dm.mbernal@gmail.com>
2021-01-30 11:33:44 -0300

    Formularios

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,7 @@
-// Es un lenguaje de programacion
-// Es del lado del cliente
-/*
-    function nombre_de_la_funcion(){
-        sentencia 1;
-        sentencia 2;
-        sentencia 3;
-    }
-*/
-
-// definir la funcion
 function holaMundo(){
     alert('Hola Mundo');
 }
-// llamar a la funcion 
-holaMundo();
 
-// variables -> let    (puede cambiar su valor)
-// constante -> const  (no puede ser cambiado su valor)
 const h1 = document.getElementsByTagName("h1");
 h1[0].addEventListener("click", function(){
     holaMundo();
