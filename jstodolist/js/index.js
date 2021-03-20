5c1e927d498748bb8a434ba67d39d041b1f766e9
Miguel Angel Bernal <dm.mbernal@gmail.com>
2021-03-20 12:05:56 -0300

    jstodolist

---
+++ jstodolist/js/index.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const title = document.getElementById('title');
+    const description = document.getElementById('description');
+    const alert = document.getElementById('alert');
+    const table = document.getElementById('table');
+    const btn = document.getElementById('add');
+
+    function addTodo() {
+        if (title.value === '' || description === '') {
+            alert.classList.remove('d-none');
+            alert.innerText = 'Title y description son requeridos';
+            return;
+        }
+        alert.classList.add('d-none');
+        const row = table.insertRow();
+        row.innerHTML = `
+            <td>${title.value}</td>
+            <td>${description.value}</td>
+            <td class="text-center">
+                <input type="checkbox">
+            </td>
+            <td class="text-right">
+                <button class="btn btn-primary mb-1">
+                    <i class="fas fa-pencil-alt"></i>
+                </button>
+                <button class="btn btn-danger mb-1 ml-1">
+                    <i class="fa fa-trash"></i>
+                </button>
+            </td>
+        `;
+        
+    }
+
+    btn.onclick = addTodo;
+
+});
